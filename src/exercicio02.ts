2cb64fd51b84b22cac5348b22f27ed9622105ff8
Ismael Veras <ismaelverass@outlook.com>
2025-07-14 10:23:28 -0300

    feat: Função que recebe um parametro do tipo number e retorna o resultado da multiplicação por 2

---
+++ src/exercicio02.ts
@@ -0,0 +1,9 @@
+// Função que recebe um parametro do tipo number e retorna o resultado da multiplicação por 2
+
+function multiplicação (valorEntrada: number){
+  return valorEntrada*2;
+}
+
+const valor = multiplicação(4);
+
+console.log(valor); 
